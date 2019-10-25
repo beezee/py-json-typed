83a64ffb28650cce7f2b2203f1b6e62b93d3129d
beezee <brian.zeligson@gmail.com>
2019-10-25 13:42:46 -0400

    run example

--- json_typed/test.py
+++ json_typed/test.py
@@ -14,3 +14,10 @@ class Foo:
 
 fbq = Combine2(Combine2(parseOptional(foo), bar, lambda x: x)().setPath(['foobar']), quux,
   lambda t: Foo(t[0][0], t[0][1], t[1]))
+
+if __name__ == '__main__':
+  print(fbq().parse('{"quux": null, "foobar": {"foo": "bar", "baz": "quux"}}'))
+  print(fbq().parse('{"quux": 4, "foobar": {"foo": "bar", "baz": "quux"}}'))
+  print(fbq().parse('{"quux": null, "foobar": {"foo": null, "baz": "quux"}}'))
+  print(fbq().parse('{"quux": 5, "foobar": {"moo": "bar", "baz": "quux"}}'))
+
